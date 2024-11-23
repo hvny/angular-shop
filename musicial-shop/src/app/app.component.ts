9e22332ae0ef2754348437ebbfbfc345648ba9b9
Vadim <elsukov.vadin@gmail.com>
2024-11-23 18:37:19 +0300

    grobal vars, fonts

--- musicial-shop/src/app/app.component.ts
+++ musicial-shop/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    // BrowserModule,
+    // BrowserAnimationsModule,
+    ButtonModule,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'musicial-shop';
