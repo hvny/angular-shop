c187666fd11ef251f0c8529685ebca6b2b67fc7d
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-12-03 12:50:24 +0300

    fix: home

--- musicial-shop/src/app/home/home.component.ts
+++ musicial-shop/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
